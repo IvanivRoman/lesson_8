5cae34a6c99f16aa6b3da3fb61b9b3d375a9ebda
Roman <ivaniv.roma.15@gmail.com>
2022-11-04 00:23:29 +0200

    create matrix trransposition and printing methods

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
+
+    public static int[][] matrixTransposition(int[][] arr) {
+        int[][] result = new int[arr[0].length][arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                result[j][i] = arr[i][j];
+            }
+        }
+        return result;
+    }
+
+    public static void printMatrix(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[0].length; j++) {
+                System.out.printf("%4d", array[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[][] array1 = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 0}};
+        int[][] array2 = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        printMatrix(array1);
+        System.out.println("********************************");
+        printMatrix(matrixTransposition(array1));
+        System.out.println("********************************");
+        printMatrix(array2);
+        System.out.println("********************************");
+        printMatrix(matrixTransposition(array2));
     }
 }
